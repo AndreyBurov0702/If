cee0bcc2a1187d36c215acd1c2d48e18e0c2aa2f
AndreyBurov0702 <129365108+AndreyBurov0702@users.noreply.github.com>
2023-03-31 20:34:09 +0500

    Add files via upload

---
+++ If.java
@@ -0,0 +1,13 @@
+public class If{
+    public static void main(String[] args){
+        int myInt=5;
+        if (myInt<10){
+            System.out.println("Andrey,ты прав");
+        }else if(myInt<20){
+            System.out.println("Andrey, ты не прав");
+        }else{
+            System.out.println("Не один из предыдущих случаев");
+        }
+    }
+}
+
